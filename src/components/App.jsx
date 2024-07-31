184494546434337e12710de1bc9c1c0b7322fcae
Divya Joshi <divyaj.redj@gmail.com>
2024-07-31 19:59:40 -0400

    changes

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import TodoInput from "./TodoInput"
+import TodoList from "./TodoList"
+
+
+function App() {
+  
+
+  return (
+    <main>
+      <TodoInput/>
+      <TodoList/>
+    </main>
+  )
+}
+
+export default App
